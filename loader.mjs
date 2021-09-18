f9232229c3956938abe391e058cea775c520bb4d
Jonas Kello <jonas.kello@gmail.com>
2021-09-18 18:05:59 +0200

    Refactor

--- loader.mjs
+++ loader.mjs
@@ -36,21 +36,22 @@ export function resolve(specifier, context, defaultResolve) {
 export async function load(url, context, defaultLoad) {
   // Return transpiled source if typescript file
   if (extensionsRegex.test(url)) {
-    // Call defaultLoad to get the source (the format of typescript files is always module)
+    // Call defaultLoad to get the source
+    const format = getTypescriptModuleFormat();
     const { source: rawSource } = await defaultLoad(
       url,
-      { format: "module" },
+      { format },
       defaultLoad
     );
-    const source = transpileTypescript(url, rawSource);
-    return { format: "module", source };
+    const source = transpileTypescript(url, rawSource, "esm");
+    return { format, source };
   }
 
   // Let Node.js load it
   return defaultLoad(url, context);
 }
 
-function transpileTypescript(url, source) {
+function transpileTypescript(url, source, outputFormat) {
   let filename = url;
   if (!isWindows) filename = fileURLToPath(url);
 
@@ -65,7 +66,7 @@ function transpileTypescript(url, source) {
     target: "esnext",
     // This sets the output format for the generated JavaScript files
     // format: format === "module" ? "esm" : "cjs",
-    format: "esm",
+    format: outputFormat,
   });
 
   if (warnings && warnings.length > 0) {
@@ -76,4 +77,16 @@ function transpileTypescript(url, source) {
   }
 
   return js;
+}
+
+function getTypescriptModuleFormat() {
+  // The format of typescript file could be ESM or CJS
+  // Since typescript always generates .js files, it can be a module if type: module is set in package.json
+  // However it can also be a module otherwise......
+  // Is it even important to know this, the source is loaded in the same way regardless.......
+  // Perhaps we cannot transpile CJS into ESM with esbuild? Then we need to know...
+  // An ECMAScript module in JS cannot use require
+  // A typescript module can use require but can it in the same module use ESM import/export?
+
+  return "module";
 }
